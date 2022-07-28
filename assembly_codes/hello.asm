2dffc38a78295f93919e17cd41b708691e237b52
Sitharam123 <110094194+Sitharam123@users.noreply.github.com>
2022-07-28 19:54:20 +0530

    Add files via upload

---
+++ assembly_codes/hello.asm
@@ -0,0 +1,15 @@
+;hello
+;using assembly language for turning LED on
+
+
+.include "/home/vedula/m328Pdef.inc"
+
+
+  ldi r16,0b00100000
+  out DDRB,r16
+  
+  
+  ldi r17,0b00000000
+  out PortB,r17
+Start:
+  rjmp Start
